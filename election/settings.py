a012500f9e4ac90e8f9bec4544665a790e848c39
KYOMO ELIEZA <kyomo89elieza@gmail.com>
2025-03-25 01:34:11 +0300

    changes to data

--- election/settings.py
+++ election/settings.py
@@ -142,18 +142,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-#STATIC_ROOT = "/opt/app1/static"
-#MEDIA_ROOT = "/opt/app1/media"
-"""LIVE WORKING"""
-#STATIC_ROOT=os.path.join(BASE_DIR, 'static')
-#MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
-"""TESTING"""
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "media"),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+
 
 #STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 """END OF TESTING"""
