37c8ff50877bda68336d40766a29f38d0be6131c
KYOMO ELIEZA <kyomo89elieza@gmail.com>
2025-03-25 02:19:58 +0300

    chnges to dat2

--- election/settings.py
+++ election/settings.py
@@ -16,7 +16,7 @@ import environ
 BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-BASE_DIR1 = Path(__file__).resolve().parent.parent
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -144,11 +144,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
-    PROJECT_DIR / "static"
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "media"),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
-
 
 #STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 """END OF TESTING"""
