970f74b39b08d8d839a003a7e2db44dc523985d9
KYOMO ELIEZA <kyomo89elieza@gmail.com>
2025-03-25 02:27:12 +0300

    changes to data

--- election/urls.py
+++ election/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 from django.urls import path
 from candidato.models import Publishing
 from django.conf.urls.static import static
-from django.conf import settings
+from election import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 if not Publishing.objects.first():
