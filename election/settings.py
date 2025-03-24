96d7665e6d0bc3be5d246f42550ea0850216f04a
KYOMO ELIEZA <kyomo89elieza@gmail.com>
2025-03-25 02:15:11 +0300

    chnages to data

--- election/settings.py
+++ election/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    PROJECT_DIR / "static",
     "/opt/projects/election/static",
 ]
 
