a422d65d026af4b82494ef1d213d3388f16c523a
KYOMO ELIEZA <kyomo89elieza@gmail.com>
2025-03-25 03:24:16 +0300

    changes to data

--- election/settings.py
+++ election/settings.py
@@ -143,14 +143,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "media"),
+]
 
-STATIC_ROOT = os.path.join(PROJECT_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
-
-#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-"""END OF TESTING"""
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
