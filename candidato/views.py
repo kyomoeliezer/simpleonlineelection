99e89c22f3424886ef30b4a807765dd84ff7008d
Kyomo <kyomo89elieza@gmail.com>
2026-09-18 07:49:19 +0300

    changes to data

--- candidato/views.py
+++ candidato/views.py
@@ -773,7 +773,7 @@ class Dashboard(LoginRequiredMixin,View):
     redirect_field_name = 'next'
     template_name='dashboard/dashboard.html'
     def get(self,request,*args,**kwargs):
-        if not request.user.role:
+        if not request.user.role and 'demouser' not in request.user.username:
             return redirect(reverse('voting'))
 
         context={}
